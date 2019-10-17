d755d3233dd05de9f1b5695c3c6040b5f7226061
israloza <isra.loza15@gmail.com>
2019-10-17 00:41:08 -0500

    GitIgnore

---
+++ storage/framework/views/0375b45f3d2fd6e1aca6ebb3490690899031011f.php
@@ -0,0 +1,17 @@
+<?php $__env->startSection('content'); ?>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Administrar notas</div>
+
+                <div class="card-body">
+                    <tareas></tareas>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /Applications/XAMPP/xamppfiles/htdocs/PROYECTOS_WEB/Laravel/Mix/resources/views/home.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/3eea95c7a37ab640adde3e21930ff0cc475aa449.php
@@ -0,0 +1,4 @@
+<?php $__env->startSection('content'); ?>
+    <example-component></example-component>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /Applications/XAMPP/xamppfiles/htdocs/PROYECTOS_WEB/Laravel/Mix/resources/views/welcome.blade.php ENDPATH**/ ?>
